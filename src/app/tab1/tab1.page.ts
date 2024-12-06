f146bc2f42d8a5fe4712a4cc418a6d9a8954460a
Radek Vala <radek.www@gmail.com>
2024-12-06 17:53:16 +0100

    UI STAG subject search

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { StagService } from "../api/stag.service";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
+  subjectAbbr: string = "";
 
-  constructor() {}
+  constructor(private stagService: StagService) {}
 
+  searchClicked() {
+    console.log(this.subjectAbbr);
+
+    this.stagService.fetchSubjecInfo("AUIUI", this.subjectAbbr);
+  }
 }

27bcf86b84813c6d6dd1bc8e85573e1316944080
Radek Vala <radek.www@gmail.com>
2024-12-07 10:14:02 +0100

    Show fetched subjects by kateda on Tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { PredmetKatedry, StagService } from "../api/stag.service";
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  selector: "app-tab3",
+  templateUrl: "tab3.page.html",
+  styleUrls: ["tab3.page.scss"],
 })
 export class Tab3Page {
+  katedra: string = "";
+  rok: string = "2024";
 
-  constructor() {}
+  data?: PredmetKatedry[];
 
+  constructor(private stagService: StagService) {}
+
+  async btnSearchClicked() {
+    this.data = await this.stagService.fetchSubjectsByKatedra(
+      this.katedra,
+      this.rok,
+    );
+  }
 }
